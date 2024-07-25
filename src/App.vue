c08efa59085b5ff0a858fc3ee9108f50925c57af
Caue Gomes de Oliveira <cauego96@hotmail.com>
2024-07-25 09:15:24 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Commentator/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Commentator from './components/Commentator.vue';
+
+@Options({
+  components: {
+    Commentator,
+  },
+})
+export default class App extends Vue {}
+</script>
---
+++ src/components/Commentator.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="comment">
+            Comment
+        </button>
+        <p>
+            {{ commentary }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+    name: 'CommentatorComponent',
+    setup() {
+        const commentary = ref('');
+
+        const comment = () => {
+            commentary.value = "Olá Vue";
+        };
+
+        return {
+            commentary,
+            comment
+        };
+    }
+});
+</script>
